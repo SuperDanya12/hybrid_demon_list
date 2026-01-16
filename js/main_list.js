7ea52a3c983ad16d3699184694cce41386779afe
SuperDanya12 <superdanya1712@gmail.com>
2026-01-16 20:37:11 +0300

    Update main_list.js

--- js/main_list.js
+++ js/main_list.js
@@ -7,13 +7,13 @@ const main_list = [
   
   {name: "EQU1NOX", author: "TW1ST3D", id: "300", image: "equ1nox.png", verifer: "player", victors: []},
   
-  {name: "ILOVEWAVE", author: "Burda5800gd", id: "408", image: "ilovewave.png", verifer: "Burda5800gd", victors: []},
+  {name: "ILOVEWAVE", author: "Burda5800gd", id: "408", image: "ilovewave.png", verifer: "Burda5800gd", victors: ["CanserTeam"]},
 
-  {name: "Evil Pebble", author: "SuperDanya12", id: "398", image: "evil_pebble.png", verifer: "SuperDanya12", victors: ["Burda5800gd"]},
+  {name: "Evil Pebble", author: "SuperDanya12", id: "398", image: "evil_pebble.png", verifer: "SuperDanya12", victors: ["Burda5800gd","CanserTeam"]},
 
-  {name: "toxic waste", author: "KraM", id: "644", image: "toxic_waste.png", verifer: "TimurBossGD", victors: []},
+  {name: "toxic waste", author: "KraM", id: "644", image: "toxic_waste.png", verifer: "TimurBossGD", victors: ["CanserTeam"]},
 
-  {name: "MAYMORY", author: "Kosmos", id: "393", image: "maymory.png", verifer: "Burda5800gd", victors: []},
+  {name: "MAYMORY", author: "Kosmos", id: "393", image: "maymory.png", verifer: "Burda5800gd", victors: ["CanserTeam"]},
 
   {name: "DIACET", author: "ZippRolex", id: "566", image: "diacet.png", verifer: "player", victors: []},
   
@@ -21,19 +21,19 @@ const main_list = [
 
   {name: "Dual Switch", author: "Kosmos", id: "639", image: "dual_switch.png", verifer: "player", victors: []},
   
-  {name: "2p Force", author: "BiseikaGMD", id: "443", image: "2p_force.png", verifer: "player", victors: []},
+  {name: "2p Force", author: "BiseikaGMD", id: "443", image: "2p_force.png", verifer: "player", victors: ["CanserTeam"]},
 
   {name: "Sine Wave", author: "Burda5800gd", id: "405", image: "sine_wave.png", verifer: "Burda5800gd", victors: []},
 
   {name: "Fractured Reality", author: "diore", id: "301", image: "fractured_reality.png", verifer: "player", victors: []},
   
-  {name: "goida epta", author: "jaggeddima", id: "429", image: "goida_epta.png", verifer: "jaggeddima", victors: []},
+  {name: "goida epta", author: "jaggeddima", id: "429", image: "goida_epta.png", verifer: "jaggeddima", victors: ["CanserTeam"]},
 
   {name: "Unbound", author: "TW1ST3D", id: "367", image: "unbound.png", verifer: "player", victors: []},
   
-  {name: "Heaven and Hell", author: "MasterZeyn4822", id: "412", image: "heaven_and_hell.png", verifer: "TimurBossGD", victors: []},
+  {name: "Heaven and Hell", author: "SnowballZn", id: "412", image: "heaven_and_hell.png", verifer: "SnowballZn", victors: ["TimurBossGD","CanserTeam"]},
 
-  {name: "THinkINg Fuck", author: "jaggeddima", id: "127", image: "thinking_fuck.png", verifer: "jaggeddima", victors: []},
+  {name: "THinkINg Fuck", author: "jaggeddima", id: "127", image: "thinking_fuck.png", verifer: "jaggeddima", victors: ["CanserTeam"]},
 
   {name: "8 Bit Game", author: "Kosmos", id: "396", image: "8_bit_game.png", verifer: "player", victors: []},
 
@@ -69,6 +69,7 @@ for (let i = 0; i < main_list.length; i++) {
   container.appendChild(clone);
 }
 original.style.display = 'none';
+
 
 
 
