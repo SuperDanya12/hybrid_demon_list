b8da86cc05e255343b1eee3bd71dc7a9bdee5512
SuperDanya12 <superdanya1712@gmail.com>
2026-01-18 19:48:14 +0300

    Update rules.js

--- js/rules.js
+++ js/rules.js
@@ -1,6 +1,26 @@
-const future_button = document.querySelector(".nav_future_button");
 const burger_button = document.querySelector(".burger_button");
 const burger_menu = document.getElementById("burger_menu");
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+const rules_box1 = document.getElementById("rules_box1")
+const rules_box2 = document.getElementById("rules_box2")
+
+
+rules_box2.style.display = 'none';
+
+button1.addEventListener('click', function() {
+  rules_box2.style.display = 'none';
+  rules_box1.style.display = 'block';
+  button1.disabled = true;
+  button2.disabled = false;
+});
+
+button2.addEventListener('click', function() {
+  rules_box1.style.display = 'none';
+  rules_box2.style.display = 'block';
+  button2.disabled = true;
+  button1.disabled = false;
+});
 
 burger_button.addEventListener('click', function() {
   if (burger_menu.style.display != 'flex') {
