b5705d916da52f88675c3a6741eeaee8b33e4f06
SuperDanya12 <superdanya1712@gmail.com>
2026-02-22 11:28:15 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,23 +1,23 @@
 const main_list = [
   {name: "Miha Difficult", creators: ["KuratheGuy","MihaMilos","Triscis","Hexy","Gamecheese"], id: "240", image: "miha_difficult.png", verifier: "Lunikay", victors: [], points: 20},
-  {name: "Also Anathema", creators: ["Burda5800gd","Kosmos","PERFARATARRRRRR"], id: "665", image: "also_anathema.png", verifier: "Burda5800gd", victors: ["Kwist"], points: 15},
+  {name: "Also Anathema", creators: ["Burda5800gd","Kosmos","PERFARATARRRRRR"], id: "665", image: "also_anathema.png", verifier: "Burda5800gd", victors: [], points: 15},
   {name: "Chaos Circles", creators: ["MiRPacK19"], id: "620", image: "chaos_circles.png", verifier: "MiRPacK19", victors: [], points: 13},
   {name: "Platinum Circles", creators: ["2Rus"], id: "180", image: "platinum_circles.png", verifier: "player", victors: [], points: 10},
   {name: "EQU1NOX", creators: ["TW1ST3D"], id: "300", image: "equ1nox.png", verifier: "player", victors: [], points: 9.5},
-  {name: "ILOVEWAVE", creators: ["Burda5800gd"], id: "408", image: "ilovewave.png", verifier: "Burda5800gd", victors: ["Kwist"], points: 9},
-  {name: "Evil Pebble", creators: ["SuperDanya12"], id: "398", image: "evil_pebble.png", verifier: "SuperDanya12", victors: ["Burda5800gd","Kwist"], points: 5},
-  {name: "toxic waste", creators: ["KraM"], id: "644", image: "toxic_waste.png", verifier: "TimurBossGD", victors: ["Kwist"], points: 4.5},
-  {name: "MAYMORY", creators: ["Kosmos"], id: "393", image: "maymory.png", verifier: "Kosmos", victors: ["Burda5800gd","Kwist"], points: 2},
+  {name: "ILOVEWAVE", creators: ["Burda5800gd"], id: "408", image: "ilovewave.png", verifier: "Burda5800gd", victors: [], points: 9},
+  {name: "Evil Pebble", creators: ["SuperDanya12"], id: "398", image: "evil_pebble.png", verifier: "SuperDanya12", victors: ["Burda5800gd"], points: 5},
+  {name: "toxic waste", creators: ["KraM"], id: "644", image: "toxic_waste.png", verifier: "TimurBossGD", victors: [], points: 4.5},
+  {name: "MAYMORY", creators: ["Kosmos"], id: "393", image: "maymory.png", verifier: "Kosmos", victors: ["Burda5800gd"], points: 2},
   {name: "DIACET", creators: ["ZippRolex"], id: "566", image: "diacet.png", verifier: "player", victors: [], points: 1.5},
   {name: "CDVG", creators: ["pushkin228"], id: "241", image: "cdvg.png", verifier: "player", victors: [], points: 1},
   {name: "Dual Switch", creators: ["Kosmos"], id: "639", image: "dual_switch.png", verifier: "Kosmos", victors: [], points: 0.99},
-  {name: "2p Force", creators: ["BiseikaGMD"], id: "443", image: "2p_force.png", verifier: "Kwist", victors: [], points: 0.97},
-  {name: "goida epta", creators: ["jaggeddima"], id: "429", image: "goida_epta.png", verifier: "jaggeddima", victors: ["Kwist"], points: 0.94},
+  {name: "2p Force", creators: ["BiseikaGMD"], id: "443", image: "2p_force.png", verifier: "player", victors: [], points: 0.97},
+  {name: "goida epta", creators: ["jaggeddima"], id: "429", image: "goida_epta.png", verifier: "jaggeddima", victors: [], points: 0.94},
   {name: "Fractured Reality", creators: ["diore"], id: "301", image: "fractured_reality.png", verifier: "player", victors: [], points: 0.93},
   {name: "Sine Wave", creators: ["Burda5800gd"], id: "405", image: "sine_wave.png", verifier: "Burda5800gd", victors: [], points: 0.9},
   {name: "Unbound", creators: ["TW1ST3D"], id: "367", image: "unbound.png", verifier: "player", victors: [], points: 0.89},
-  {name: "Heaven and Hell", creators: ["SnowballZn"], id: "412", image: "heaven_and_hell.png", verifier: "SnowballZn", victors: ["TimurBossGD","Kwist"], points: 0.81},
-  {name: "THinkINg Fuck", creators: ["jaggeddima"], id: "127", image: "thinking_fuck.png", verifier: "jaggeddima", victors: ["Burda5800gd","Kwist"], points: 0.75},
+  {name: "Heaven and Hell", creators: ["SnowballZn"], id: "412", image: "heaven_and_hell.png", verifier: "SnowballZn", victors: ["TimurBossGD"], points: 0.81},
+  {name: "THinkINg Fuck", creators: ["jaggeddima"], id: "127", image: "thinking_fuck.png", verifier: "jaggeddima", victors: ["Burda5800gd"], points: 0.75},
   {name: "8 Bit Game", creators: ["Kosmos"], id: "396", image: "8_bit_game.png", verifier: "Kosmos", victors: [], points: 0.67},
   {name: "prism attack", creators: ["chaynik"], id: "582", image: "prism_attack.png", verifier: "player", victors: [], points: 0.65},
   {name: "DzhunGarik", creators: ["diore"], id: "316", image: "dzhungarik.png", verifier: "player", victors: [], points: 0.6},
@@ -29,8 +29,9 @@ const main_list = [
 
 const future_list = [
   {name: "ILOVEWAVE II", author: "Burda5800gd", image: "ilovewave_ii.png", status: "Verifying"},
-  {name: "Hybrid Avernus", author: "Kwist", image: "hybrid_avernus.png", status: "Building"},
+  {name: "Hybrid Avernus", author: , image: "hybrid_avernus.png", status: "Building"},
   {name: "Toxic Waste Infinity", author: "SuperDanya12", image: "toxic_waste_infinity.png", status: "Building"},
   {name: "High Tide", author: "SnowballZn", image: "high_tide.png", status: "Building"},
   {name: "Azurema", author: "SnowballZn", image: "azurema.png", status: "Building"}
 ];
+
